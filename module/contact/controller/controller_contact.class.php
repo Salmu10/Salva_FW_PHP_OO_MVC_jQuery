8c751dff6253f00140ef64fe8f46ac5b6f16e3c3
Salmu10 <Ubedasalmu@gmail.com>
2022-05-11 18:51:53 +0200

    Correccion modulo contact

--- module/contact/controller/controller_contact.class.php
+++ module/contact/controller/controller_contact.class.php
@@ -7,19 +7,19 @@
 		
 		function send_contact_us(){
 
-			echo json_encode('Done!');
+			$message = ['type' => 'contact',
+						'inputName' => $_POST['name'], 
+						'fromEmail' => $_POST['email'], 
+						'inputMatter' => $_POST['matter'], 
+						'inputMessage' => $_POST['message']];
+			$email = json_decode(mail::send_email($message), true);
 
-			// $message = ['type' => 'contact',
-			// 			'inputName' => $_POST['name'], 
-			// 			'fromEmail' => $_POST['email'], 
-			// 			'inputMatter' => $_POST['matter'], 
-			// 			'inputMessage' => $_POST['message']];
-			// $email = json_decode(mail::send_email($message), true);
-			// if (!empty($email)) {
-			// 	echo json_encode('Done!');
-			// 	return;  
-			// } 
-			// echo json_encode('Error!');
+			if (!empty($email)) {
+				echo json_encode('Done!');
+				return;  
+			} else {
+				echo json_encode('Error!');
+			}
 		}
 	}
 ?>
